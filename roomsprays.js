048a7b05ba12810fad247f90e84499e612faa156
ranajit das <dasranajit410@gmail.com>
2022-05-09 17:16:55 +0530

    Home fragnance and new file added

---
+++ roomsprays.js
@@ -0,0 +1,192 @@
+var roomspraysData=[
+    {
+        Product_img:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw5cb847c3/crop/026397112_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        Product_name:"Eucalyptus Spearmint",
+        productId:"Concentrated Room Sprays",
+        price:8.5
+
+    },
+    {
+        Product_img:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw026dbe98/crop/026410608_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        Product_name:"Fresh Cut Lilacs",
+        productId:"Concentrated Room Sprays",
+        price:8.5
+
+    },{
+        Product_img:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw0b3cf307/crop/026454382_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        Product_name:"Blue Rashperry",
+        productId:"Concentrated Room Sprays",
+        price:8.5
+
+    },
+    {
+        Product_img:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw280a985a/crop/026501106_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        Product_name:"Laundry Dry",
+        productId:"Concentrated Room Sprays",
+        price:7.5
+
+    },
+    {
+        Product_img:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw41dfae81/crop/026438912_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        Product_name:"Ocean Driftwood",
+        productId:"Concentrated Room Sprays",
+        price:9.5
+
+    },
+    {
+        Product_img:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwbe0d1b45/crop/026501105_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        Product_name:"Mermaid Lagoon",
+        productId:"Concentrated Room Sprays",
+        price:8.5
+
+    },{
+        Product_img:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw0e563d1e/crop/026454381_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        Product_name:"The Perfect Summer",
+        productId:"Concentrated Room Sprays",
+        price:8.5
+
+    },
+    {
+        Product_img:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw59522c25/crop/026397114_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        Product_name:"Endless Weekend",
+        productId:"Concentrated Room Sprays",
+        price:7.5
+
+    },
+    {
+        Product_img:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dweab96098/crop/026454385_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        Product_name:"Champagne Lemon Pound Cake",
+        productId:"Concentrated Room Sprays",
+        price:8.5
+
+    },
+    {
+        Product_img:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwbce4b113/crop/026454383_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        Product_name:"Mango Mai Tai",
+        productId:"Concentrated Room Sprays",
+        price:8.5
+
+    },{
+        Product_img:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw8e77e52d/crop/026438909_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        Product_name:"Island Margarita",
+        productId:"Concentrated Room Sprays",
+        price:8.5
+
+    },
+    {
+        Product_img:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw379e5935/crop/026465839_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        Product_name:"Warm Ocean Breez",
+        productId:"Concentrated Room Sprays",
+        price:8.5
+
+    },
+    {
+        Product_img:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwc803ff9d/crop/026410611_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        Product_name:"Butterfly",
+        productId:"Concentrated Room Sprays",
+        price:8.3
+
+    },
+    {
+        Product_img:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw881e45ac/crop/026410631_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        Product_name:"Raspberries",
+        productId:"Concentrated Room Sprays",
+        price:8.5
+
+    },{
+        Product_img:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwfc594c6f/crop/026410616_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        Product_name:"Chocolate Pound Cake",
+        productId:"Concentrated Room Sprays",
+        price:8.5
+
+    },
+    {
+        Product_img:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw303e067a/crop/026410630_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        Product_name:"Tortoil Pound Cake",
+        productId:"Concentrated Room Sprays",
+        price:8.5
+
+    },
+    {
+        Product_img:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw2b6afa90/crop/026454384_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        Product_name:"Palo Santo",
+        productId:"Concentrated Room Sprays",
+        price:8.5
+
+    },
+    {
+        Product_img:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf1f18944/crop/026410629_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        Product_name:"Euclayptus Rain",
+        productId:"Concentrated Room Sprays",
+        price:8.5
+
+    },{
+        Product_img:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw7f246337/crop/026410610_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        Product_name:"Blue Rashperry Cone",
+        productId:"Concentrated Room Sprays",
+        price:8.5
+
+    },
+    {
+        Product_img:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw58be47b1/crop/026412037_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        Product_name:"Coconut Colada",
+        productId:"Concentrated Room Sprays",
+        price:8.5
+
+    }
+]
+var addDatatoCart=JSON.parse(localStorage.getItem("cart-Data")) || [];
+roomspraysData.forEach(function (elem){
+    var box=document.createElement("div");
+
+    var Product_img=document.createElement("img");
+    Product_img.setAttribute("src",elem.Product_img);
+    Product_img.setAttribute("class","canimg");
+
+    var sty=document.createElement("h4");
+    sty.style.color="blue";
+    sty.innerText="New Fragrance";
+
+    var Product_name=document.createElement("h3");
+    Product_name.innerText=elem.Product_name;
+
+    var productId=document.createElement("h6");
+    productId.innerText=elem.productId;
+
+    var price=document.createElement("h3");
+    price.innerText="$"+elem.price;
+
+    var button=document.createElement("button");
+    button.innerText="Add To BAG";
+    button.addEventListener("click",function(){
+        addToCart(elem)
+    })
+
+    var starimg=document.createElement("img");
+    starimg.setAttribute("src","download.png");
+    starimg.setAttribute("class","starimg");
+
+    box.append(Product_img,sty,Product_name,productId,price,button,starimg);
+
+    document.querySelector("#grid").append(box);
+})
+console.log(candlesData)
+
+function addToCart(elem){
+    var found=0;
+    for(var i=0;i<addDatatoCart.length;i++){
+        if(elem == addDatatoCart[i]){
+            found=1;
+            break;
+        }
+    }
+    if(found==0){
+        addDatatoCart.push(elem)
+        localStorage.setItem("cart-Data",JSON.stringify(addDatatoCart))
+        alert("Product added sucessfully")
+    }
+    else{
+        alert("Product already Added")
+    }
+}
+
